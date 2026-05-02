bdcee56313b5005138418d7b591497045b364891
mikey1th <mikey@middlegroundmade.com>
2026-05-02 12:16:16 -0400

    added rephrases gearing up for design projects!

---
+++ src/data/rephrases-data.js
@@ -0,0 +1,31 @@
+
+
+const rephrases =[
+    {
+        original: "Are you going to plan or are you going to react?",
+        improved: "I’m going to plan to react.",
+    },
+
+    {
+        original: "Once I figure out what I want to say then I’ll start to write?",
+        improved: "No, you have to write in order to figure out what you want to say.",
+    },
+
+    {
+        original: "We don’t have a work-culture.",
+        improved: "Our culture is work.",
+    },
+
+    {
+        original: "We don’t have to make more thin places.",
+        improved: "If instead we choose to make more places thin.",
+    },
+
+    {
+        original: "I’m not better because this thing is done.",
+        improved: "I’m better because I’ve done this thing.",
+    },
+
+]
+
+export default rephrases
