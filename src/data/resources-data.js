f44f359f7e63d5ba35984fd50c99292a89de6623
mikey1th <mikey@middlegroundmade.com>
2026-05-07 22:52:48 -0400

    finished newsletter pages, which include sign up and resources only a few big things left then its jut populating content

---
+++ src/data/resources-data.js
@@ -0,0 +1,91 @@
+
+
+
+const resources =[
+    {
+        number: 128,
+        text: "Adrian Chikovsky, Children of Time",
+        link: "https://tinyurl.com/24shmysp",
+        issue: "Issue 128",
+  
+    },
+
+    {
+        number: 127,
+        text: "Vulfpeck, live at Red Rocks",
+        link: "https://www.youtube.com/watch?v=1LkYJl5EdNY",
+        issue: "Issue 127"
+    },
+
+    {
+        number: 126,
+        text: "William Zinsser, On Writing Well",
+        link: "https://tinyurl.com/3yz7m74wY",
+        issue: "Issue 126",
+    },
+
+    {
+        number: 121,
+        text: "John Steinbeck Pigasus",
+        link: "https://www.sjsu.edu/steinbeck/resources/biography/pigasus.php",
+        issue: "Issue 121"
+    },
+
+    {
+        number: 118,
+        text: "Peirce Brown, Red Rising",
+        link: "https://tinyurl.com/5ath36bn",
+        issue: "Issue 118"
+    },
+
+    {
+        number: 116,
+        text: "Than Average, a comparison websote",
+        link: "https://thanaverage.xyz/",
+        issue: "Issue 116"
+    },
+
+    {
+        number: 115,
+        text: "The best weather website on the internet",
+        link: "https://wttr.in/",
+        issue: "Issue 115"
+    },
+
+    {
+        number: 113,
+        text: "A quote from Viktor Frankl's Man's Search for Meaning",
+        link: "https://tinyurl.com/4zmumm4r",
+        quote: `"When we are no longer able to change a situation-just think of an incurable disease such as inoperable 
+        cancer—we are challenged to change ourselves."`,
+        issue: "Issue 113"
+
+    },
+
+    {
+        number: 100,
+        text: "An essay from Oliver Burkeman about turbulent times",
+        link: "https://ckarchive.com/b/p9ueh9h2dx5gksm6ggw6kapn5o333hr",
+        issue: "Issue 100"
+
+    },
+
+    {
+        number: 97,
+        text: "Amusing Ourselves to Death, by Neil Postman",
+        link: "https://tinyurl.com/5n7j8a89",
+        issue: "Issue 97"
+
+    },
+
+    {
+        number: 84,
+        textNoLink: "A quote from Lester Beal",
+        issue: "Issue 95",
+        quote: `"The way a man lives is essential to the work he produces. The two cannot be separated."`
+
+    },
+
+]
+
+export default resources
