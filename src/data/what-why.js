83ac63b995727e3ae7652df125034e5963c76dd8
mikey1th <mikey@middlegroundmade.com>
2026-05-10 01:13:18 -0400

    added what /why finished newsletter added more links for about

---
+++ src/data/what-why.js
@@ -0,0 +1,19 @@
+
+
+const content = [
+
+    {   
+        label: "What",
+        text: "The short verison is that Middle Ground Made, as mentioned earlier is an heirloom design studio. But that, in an of itself is not a helpful explination. An heirloom is soething we think we should hold on to. Sometimes its because the artifact holds sentimential value. But I think the the starting place of an heirloom is design and ntntiaonality. Heirlooms don't just happen. Somone considers each element and carefully crafts it"
+    },
+    
+    {
+        label: "Why",
+        text: "The short verison is that Middle Ground Made, as mentioned earlier is an heirloom design studio. But that, in an of itself is not a helpful explination. An heirloom is soething we think we should hold on to. Sometimes its because the artifact holds sentimential value. But I think the the starting place of an heirloom is design and ntntiaonality. Heirlooms don't just happen. Somone considers each element and carefully crafts itncncncjcncjncejn TESTTTTT",
+    }
+    
+
+]
+    
+
+export default content;
