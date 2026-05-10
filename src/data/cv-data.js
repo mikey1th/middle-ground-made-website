7e9f63b28726c12187495cf2487cbd8f85ec0d6d
mikey1th <mikey@middlegroundmade.com>
2026-05-10 16:56:07 -0400

    added cv, bio, cleand up email footer

---
+++ src/data/cv-data.js
@@ -0,0 +1,76 @@
+const items = [
+    {
+        year: "2023 - Now",
+        title: "Open Notes",
+        link: "../pages/open-notes",
+        description: "A collection of obersvational essays and fantastical myths.",
+        sortYear: 2023,
+        type: "project",
+    },
+
+    {
+        year: "2023 - Now",
+        title: "Bookshop",
+        link: "../pages/open-notes",
+        description: "A place to buy Middle Ground Made books and merch.",
+        sortYear: 2023,
+        type: "project",
+    },
+
+    {
+        year: "2021 - Now",
+        title: "Compendium",
+        link: "../pages/open-notes",
+        description: "A collection of one-off design projects.",
+        sortYear: 2021,
+        type: "project",
+    },
+
+    {
+        year: "2021 - Now",
+        title: "Middle Ground Newsletter",
+        link: "../pages/open-notes",
+        description: "A monthly newsletter sharing a note and a resource",
+        sortYear: 2021,
+        type: "project",
+    },
+
+    {
+        year: "2021 - Now",
+        title: "Middle Ground Made Website",
+        link: "../pages/open-notes",
+        description: "The internet isalnd for all things Middle Ground Made",
+        sortYear: 2021,
+        type: "project",
+    },
+
+    {
+        year: "2021 - 2025",
+        title: "Lead Collective, Creative Director",
+        link: "../pages/open-notes",
+        description: "Ideating, and executing various creative ideas rooted in design, marketing, and story telling.",
+        sortYear: 2021,
+        type: "work",
+    },
+
+    {
+        year: "2025 - Now",
+        title: "Bitwise Asset Managers, Marketing Designer",
+        link: "../pages/open-notes",
+        description: "Ideating, and executing various creative ideas rooted in design, marketing, and story telling.",
+        sortYear: 2025,
+        type: "work",
+    },
+
+    {
+        year: "2017 - 2020",
+        title: "North Greenville University",
+        link: "../pages/open-notes",
+        description: "Bachelor's of Science in Marketing",
+        sortYear: 2017,
+        type: "education",
+    },
+
+]
+    
+export default items;
