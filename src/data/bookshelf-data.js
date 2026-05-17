089fa285d81c0e6e789eb9cb191fc016f35f4cf9
mikey1th <mikey@middlegroundmade.com>
2026-05-17 09:31:00 -0400

    got rid of stacks, no one cares.Replaced with bookshelf, I think everything is offically roughed in. Time to fine tooth comb this thing add content ans actually ship.

---
+++ src/data/bookshelf-data.js
@@ -0,0 +1,30 @@
+const items = [
+
+    {
+        titleAuthor: "Walden, Henry David Thoreau",
+        link: "../pages/open-notes",
+        novelNonFitcion: "non-fiction",
+        genre: "philosphy",
+        description: "Huge book for me, something about those transcendntalists, ami right??"
+    },
+
+    {
+        titleAuthor: "On Writing Well, William Zinnser",
+        link: "../pages/open-notes",
+        novelNonFitcion: "non-fiction",
+        genre: "craft",
+        description: "Huge book for me, something about those transcendntalists, ami right??"
+    },
+
+    {
+        titleAuthor: "Children of Time, Adrian Chaikovsky",
+        link: "../pages/open-notes",
+        novelNonFitcion: "novel",
+        genre: "Sci-Fi",
+        description: "Huge book for me, something about those transcendntalists, ami right??"
+    },
+
+
+]
+    
+export default items;
