d11495e3d7efc6f0ecc339c67146400aa7f87288
mikey1th <mikey@middlegroundmade.com>
2026-06-04 15:36:10 -0400

    new copy for pages, added MGC images

--- src/data/what-why.js
+++ src/data/what-why.js
@@ -3,12 +3,12 @@
 const content = [
 
     {   
-        label: "What",
+        label: "What is Middle Ground and What is an Heirloom",
         text: "The short verison is that Middle Ground Made, as mentioned earlier is an heirloom design studio. But that, in an of itself is not a helpful explination. An heirloom is soething we think we should hold on to. Sometimes its because the artifact holds sentimential value. But I think the the starting place of an heirloom is design and ntntiaonality. Heirlooms don't just happen. Somone considers each element and carefully crafts it"
     },
     
     {
-        label: "Why",
+        label: "Why is Middle Ground the way it is",
         text: "The short verison is that Middle Ground Made, as mentioned earlier is an heirloom design studio. But that, in an of itself is not a helpful explination. An heirloom is soething we think we should hold on to. Sometimes its because the artifact holds sentimential value. But I think the the starting place of an heirloom is design and ntntiaonality. Heirlooms don't just happen. Somone considers each element and carefully crafts itncncncjcncjncejn TESTTTTT",
     }
     
