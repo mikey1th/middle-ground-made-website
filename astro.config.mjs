0b49078107e7fe515bb8f065bec5c8350b809a75
mikey1th <mikey@middlegroundmade.com>
2026-06-23 17:49:49 -0400

    installed fontaine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { FontaineTransform } from 'fontaine';
 
 
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: ['Arial', 'Helvetica Neue', 'sans-serif'],
+        resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
+      }),
+    ],
+  },
+});
