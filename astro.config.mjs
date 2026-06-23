5c1afcbd5231d51177a20b76744048d7d897020c
mikey1th <mikey@middlegroundmade.com>
2026-06-23 17:58:32 -0400

    removed fontaine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { FontaineTransform } from 'fontaine';
+
 
 
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [
-      FontaineTransform.vite({
-        fallbacks: ['Arial', 'Helvetica Neue', 'sans-serif'],
-        resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
-      }),
-    ],
-  },
-});
+export default defineConfig({});
+
+
